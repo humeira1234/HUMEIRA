245cef8ec213f1b44f86ced66fa062f954c1b490
humeira1234 <uzerirashid1930@gmail.com>
2024-09-05 12:48:58 +0300

    this a js file

---
+++ script.js
@@ -0,0 +1,15 @@
+// Global Wndow Object
+console.log(window);
+
+//document Object
+console.log(document);
+
+// title
+console.log(document.title);
+
+// add a h3 that says hello from Js
+document.getElementById("main") .innerHTML += "<h3> Hello From Js </h3>";
+
+// adding  TEXT ON THE H1
+document.querySelector("#main h1").innerHTML += " Hello ";
+
